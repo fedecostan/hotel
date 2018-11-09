a0757c977f6d5ca5c91b4c5c3175ee90c65cbe5d
fedecostan <costantino.fe@gmail.com>
2018-11-09 12:31:07 -0300

    Initial Commit

--- src/main/resources/static/js/controllers/facturaControllers.js
+++ src/main/resources/static/js/controllers/facturaControllers.js
@@ -6,8 +6,11 @@
             var facturaUrl = '../facturaController/';
 
             $scope.factura = [];
+            $scope.nuevoArticulo = [];
+            $scope.listaArticulos = [];
 
             $scope.factura.fecha = new Date();
+            $scope.factura.nroComprobante = '00000047';
 
             $scope.direccion = 'Calle 1589 - CP AA1111 - CABA';
             $scope.telefono = 'Tel. 236-1258-1245';
@@ -16,6 +19,18 @@
                 {value:'NC',name:'Nota de Credito C'},
                 {value:'DC',name:'Nota de Debito C'}
             ];
+            $scope.situacionesIva = [
+                {value:'111',name:'1111 IVA'},
+                {value:'222',name:'2222 IVA'},
+                {value:'333',name:'3333 IVA'}
+            ];
+            $scope.condicionesVenta = [
+                {value:'111',name:'1111 VENTA'},
+                {value:'222',name:'2222 VENTA'},
+                {value:'333',name:'3333 VENTA'}
+            ];
+            $scope.factura.listaPrecio = 'C';
+
 
 //            $http({method: 'GET',url: facturaUrl + 'obtenerInformacionCentro'}).then(
 //                function successCallback(response) {
@@ -23,6 +38,22 @@
 //                    $scope.telefono = 'Tel. 236-1258-1245';
 //                }, function errorCallback(response) {
 //            });
+
+
+            $scope.guardarArticulo = function () {
+                var nuevo = {
+                    fecha:$scope.nuevoArticulo.fecha,
+                    codigo:$scope.nuevoArticulo.articulo,
+                    descripcion:'XXX',
+                    cantidad:$scope.nuevoArticulo.cantidad,
+                    precio:$scope.nuevoArticulo.precio,
+                    total:'YYY'
+                }
+                $scope.listaArticulos.push(nuevo);
+                $scope.articulosTable = new NgTableParams({}, { dataset: $scope.listaArticulos});
+            };
+
+
             $scope.enviar = function () {
                 console.log($scope.factura.fecha);
                 console.log($scope.factura.tipoComprobante);
@@ -30,6 +61,10 @@
                 console.log($scope.factura.nroComprobante);
                 console.log($scope.factura.sindicato);
                 console.log($scope.factura.afiliado);
+                console.log($scope.factura.situacionIVA);
+                console.log($scope.factura.condicionVenta);
+                console.log($scope.factura.bonificacion);
+                console.log($scope.factura.listaPrecio);
             }
         })
 }());
